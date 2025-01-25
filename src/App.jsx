532b33b9caeccd1c93ae0e95cb39efa577e5e6f8
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-01-25 12:52:09 +0530

    Landing Page Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Category from "./Components/Category"
+import Food from "./Components/Food"
+import HeadCards from "./Components/HeadCards"
+import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <HeadCards/>
+      <Food/>
+      <Category/>
     </div>
   )
 }
---
+++ src/Components/HeadCards.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const HeadCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        <div className='rounded-xl relative'>
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>Sun's out, BOGO's out</p>
+                <p className='px-2'>Through 8/26</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Rm9vZHN8ZW58MHx8MHx8fDA%3D" alt="" className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'/>
+        </div>
+        <div className='rounded-xl relative'>
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
+                <p className='px-2'>Tasty Treats</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.unsplash.com/photo-1524391931851-7bdd18f138c3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGx1bmNofGVufDB8fDB8fHww" alt="" className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'/>
+        </div>
+        <div className='rounded-xl relative'>
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+                <p className='px-2'>Added Daily</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D" alt="" className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'/>
+        </div>
+    </div>
+  )
+}
+
+export default HeadCards
