8e18e011eff5029216971588e763102846b8d96b
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-02-16 03:19:19 +0530

    UI improvement

--- client/src/Components/HeadCards.jsx
+++ client/src/Components/HeadCards.jsx
@@ -5,7 +5,7 @@ const HeadCards = () => {
     <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
         <div className='rounded-xl relative'>
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                <p className='font-bold text-2xl px-2 pt-4'>Sun's out, BOGO's out</p>
+                <p className='font-bold text-2xl px-2 pt-4'>Sun&apos;s out, BOGO&apos;s out</p>
                 <p className='px-2'>Through 8/26</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
