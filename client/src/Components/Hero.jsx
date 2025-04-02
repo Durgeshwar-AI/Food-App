7c83e141ea8a1340c969ef6802751210d8ca234f
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-04-02 22:48:08 +0530

    Test

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Foods</span> Delivered</h1>
             </div>
-            <img src="https://images.pexels.com/photos/958546/pexels-photo-958546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='w-full object-cover'/>
+            <img src="https://images.pexels.com/photos/958546/pexels-photo-958546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" className='w-full object-cover'/>
         </div>
     </div>
   )
