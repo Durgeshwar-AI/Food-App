11653baa2c636763c985a07450cd4f03d5286e68
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-04-30 12:04:37 +0530

    Expanding cards

--- client/src/Components/About/ExpandingImages.jsx
+++ client/src/Components/About/ExpandingImages.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const ExpandingImages = () => {
   return (
-    <section className='flex justify-between p-8'>
-    <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" className='w-[200px] h-[400px] object-fit-cover rounded-4xl cursor-pointer transition-all delay-50 ease-in-out hover:w-[400px]'/>
-    <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" className='w-[200px] h-[400px] object-fit-cover rounded-4xl cursor-pointer transition-all delay-50 ease-in-out hover:w-[400px]'/>
-    <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" className='w-[200px] h-[400px] object-fit-cover rounded-4xl cursor-pointer transition-all delay-50 ease-in-out hover:w-[400px]'/>
-    <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" className='w-[200px] h-[400px] object-fit-cover rounded-4xl cursor-pointer transition-all delay-50 ease-in-out hover:w-[400px]'/>
-    <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" className='w-[200px] h-[400px] object-fit-cover rounded-4xl cursor-pointer transition-all delay-50 ease-in-out hover:w-[400px]'/>
-  </section>
-  )
-}
+    <section className="flex justify-between p-8 gap-2">
+      <div className="w-[200px] h-[400px] bg-cover bg-center rounded-4xl cursor-pointer transition-all duration-100 ease-in-out hover:w-[400px] bg-[url(https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg)] flex flex-col justify-end items-center p-[50px] group">
+        <h1 className="text-white font-bold hidden group-hover:block delay-100 ease-in">Durgeshwar</h1>
+        <p className="text-white hidden group-hover:block delay-100 ease-in">Student</p>
+      </div>
+      <div className="w-[200px] h-[400px] bg-cover bg-center rounded-4xl cursor-pointer transition-all duration-100 ease-in-out hover:w-[400px] bg-[url(https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg)] flex flex-col justify-end items-center p-[50px] group">
+        <h1 className="text-white font-bold hidden group-hover:block delay-100 ease-in">Durgeshwar</h1>
+        <p className="text-white hidden group-hover:block delay-100 ease-in">Student</p>
+      </div>
+      <div className="w-[200px] h-[400px] bg-cover bg-center rounded-4xl cursor-pointer transition-all duration-100 ease-in-out hover:w-[400px] bg-[url(https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg)] flex flex-col justify-end items-center p-[50px] group">
+        <h1 className="text-white font-bold hidden group-hover:block delay-100 ease-in">Durgeshwar</h1>
+        <p className="text-white hidden group-hover:block delay-100 ease-in">Student</p>
+      </div>
+      <div className="w-[200px] h-[400px] bg-cover bg-center rounded-4xl cursor-pointer transition-all duration-100 ease-in-out hover:w-[400px] bg-[url(https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg)] flex flex-col justify-end items-center p-[50px] group">
+        <h1 className="text-white font-bold hidden group-hover:block delay-100 ease-in">Durgeshwar</h1>
+        <p className="text-white hidden group-hover:block delay-100 ease-in">Student</p>
+      </div>
+      <div className="w-[200px] h-[400px] bg-cover bg-center rounded-4xl cursor-pointer transition-all duration-100 ease-in-out hover:w-[400px] bg-[url(https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg)] flex flex-col justify-end items-center p-[50px] group">
+        <h1 className="text-white font-bold hidden group-hover:block delay-100 ease-in">Durgeshwar</h1>
+        <p className="text-white hidden group-hover:block delay-100 ease-in">Student</p>
+      </div>
+    </section>
+  );
+};
 
-export default ExpandingImages
+export default ExpandingImages;
