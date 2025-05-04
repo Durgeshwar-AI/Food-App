7937324038a80fc9e343497050e28d0058cfa534
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-05-04 12:34:18 +0530

    Info section added

---
+++ client/src/Components/About/Info.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <>
+        <div className='flex flex-col p-20 gap-10'>
+            <div className='text-2xl font-bold self-start w-[600px] max-w-full'>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam eveniet dolore incidunt quidem doloremque ad id nihil distinctio sunt eos. Dignissimos, porro autem obcaecati laudantium at alias odio ex pariatur?</p>
+            </div>
+            <div className='text-lg font-semibold self-end w-[600px] max-w-full'>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda dicta necessitatibus officia impedit sint, commodi rem, quisquam ut voluptatibus rerum laudantium quibusdam a perferendis possimus magni totam voluptate neque eius?
+                </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Info
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ExpandingImages from '../Components/About/ExpandingImages'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
-import Hero from '../Components/About/Hero'
+import React from "react";
+import ExpandingImages from "../Components/About/ExpandingImages";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Hero from "../Components/About/Hero";
+import Info from "../Components/About/Info";
 
 const About = () => {
   return (
     <>
-        <Navbar/>
-        <div className='mt-16'>
-          <Hero/>
-        <ExpandingImages/>
-        </div>
-        <Footer/>
+      <Navbar />
+      <div className="mt-16">
+        <Hero />
+        <Info />
+        <ExpandingImages />
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
