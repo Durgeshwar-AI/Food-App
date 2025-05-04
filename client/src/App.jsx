17eeb6ae68e774bd17125f905c58f40a87db916c
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-05-04 12:26:14 +0530

    About hero added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 
-
 const App = () => {
   return (
       <Routes>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ExpandingImages from '../Components/About/ExpandingImages'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Hero from '../Components/About/Hero'
 
 const About = () => {
   return (
     <>
         <Navbar/>
-        <div className='mt-16'><ExpandingImages/></div>
+        <div className='mt-16'>
+          <Hero/>
+        <ExpandingImages/>
+        </div>
         <Footer/>
     </>
   )
